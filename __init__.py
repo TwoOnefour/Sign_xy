0fd488b93330ae03f9d6db5d9a5f3a50530a1d8b
TwoOnefour <lys214412@gmail.com>
2023-04-24 00:06:29 +0800

    更新刷视频，支持签到循环，添加选项

--- __init__.py
+++ __init__.py
@@ -2,11 +2,15 @@ import requests
 import os
 import execjs
 import json
+import time
+
 
 class Sign_xy:
     def __init__(self):
         # urllib3.disable_warnings()
+        self.type = None
         self.webdriver = None
+        self.times = None
         self.authorization = ""
         self.headers = {
             "schoolcertify":"10497", # 学校对应代码
@@ -25,8 +29,8 @@ class Sign_xy:
         self.url = "https://{}".format(self.headers["Host"])
         self.sessions = requests.Session()
         self.account = {
-            "username": "",  # 填入你自己的学号
-            "password": ""  # 填入你自己的密码
+            "username": "",
+            "password": ""
         }
 
     def login(self):
@@ -74,14 +78,29 @@ class Sign_xy:
         return self.sessions.get("https://{}/api/jw-starcmooc/user/currentUserInfo".format(self.headers["Host"]),verify=False)
 
     def sign(self):
-        for i in self.getGroup_id():  # 暂且写为所有课程都签到一遍
-            # self.getRegister_id(i)
-            result = self.getRegister_id(i.strip("\n"))
-            if result["data"]["signing_register"] != []:
-                self.sessions.post("https://{}/api/jx-iresource/register/sign".format(self.headers["Host"]), json={
-                    "check_type": "1",
-                    "register_id": i.strip("\n")
-                }, verify=False)
+        if not self.times:
+            self.times = 1
+        else:
+            self.times = int(self.times)
+        for times in range(self.times):
+            for i in self.getGroup_id():  # 暂且写为所有课程都签到一遍
+                # self.getRegister_id(i)
+                result = self.getRegister_id(i.strip("\n"))
+                if result["data"]["signing_register"] != []:
+                    result1 = self.sessions.post("https://{}/api/jx-iresource/register/sign".format(self.headers["Host"]), json={
+                        "check_type": "1",
+                        "register_id": i.strip("\n")
+                    }, verify=False)
+                    result1 = json.loads(result1.text)
+                    if result1["code"] == 0:
+                        print("{}   {}签到成功".format(str(datetime.datetime.now())[0:-7], result["data"]["group_name"]))
+                    elif result1["code"] == 50011:
+                        print("{}   {}已经签到过了".format(str(datetime.datetime.now())[0:-7], result["data"]["group_name"]))
+                    else:
+                        print("{}   {}{}".format(str(datetime.datetime.now())[0:-7], result["data"]["group_name"],
+                                                 result1["message"]))
+            time.sleep(60)  # 不建议改动
+
             # else:
             #     print("此课程不需要签到")
 
@@ -89,6 +108,43 @@ class Sign_xy:
         return json.loads(self.sessions.get(
             "https://{}/api/jx-iresource/course/getOpenCourse?group_id={}&is_in_course=2".format(self.headers["Host"],
                 group_id), verify=False).text)
+
+    def get_tasks(self, group_id):
+        result = self.sessions.get("https://ccnu.ai-augmented.com/api/jx-stat/group/task/queryTaskNotices", params={
+            "group_id": group_id,
+            "role": 1
+        })
+        return json.loads(result.text)["data"]["student_tasks"]
+
+    def finish_media(self):
+        try:
+            count = 0
+            for j in self.getGroup_id():
+                tasks = self.get_tasks(j.strip("\n"))
+                for i in tasks:
+                    if i["task_type"] == 1 and i["finish"] == 0:
+                        result = self.sessions.get("https://ccnu.ai-augmented.com/api/jx-iresource/vod/duration/{}".format(i["quote_id"])).text
+                        result = json.loads(result)
+                        if not result["success"]:
+                            continue
+                        result = result["data"]
+                        watched_duration = max(0, float(result["duration"]) - float(result["watched_duration"]) + 1)
+                        self.sessions.post("https://ccnu.ai-augmented.com/api/jx-iresource/vod/duration/{}".format(i["quote_id"]), json={
+                            "media_type": result["media_type"],  # 类型为录音
+                            "duration": result["duration"],  # 总时间
+                            "played": 1,  # 播放次数
+                            "watched_duration": watched_duration  # 已经看过的时长
+                        })
+                        result = self.sessions.post("https://ccnu.ai-augmented.com/api/jx-iresource/vod/checkTaskStatus", json={
+                            "task_id": i["task_id"],
+                            "media_id": i["quote_id"],
+                            "assign_id": i["assign_id"],
+                            "group_id": i["group_id"]
+                        })
+                        count += 1
+            print(f"完成了{count}个作业")
+        except Exception as e:
+            print(str(type(e)) + ":" + str(e))
 
     def getGroup_id(self):
         if os.path.exists("./group_id.txt"):
@@ -106,6 +162,23 @@ class Sign_xy:
         return result
 
     def run(self):
+        if not self.type:
+            return
+        if self.account["username"] == "" or self.account["password"] == "":
+            if os.path.exists(os.path.split(os.path.realpath(__file__))[0] + "/account.txt"):
+                with open(os.path.split(os.path.realpath(__file__))[0] + "/account.txt", "r") as f:
+                    account_list = f.readlines()
+                    self.account["username"] = account_list[0].strip("\n").strip(" ")
+                    self.account["password"] = account_list[1].strip("\n").strip(" ")
+            else:
+                print("请填入账号密码。注意：账号密码为你统一身份认证的账号密码")
+                self.account["username"] = input("学号：")
+                self.account["password"] = input("密码：")
+                with open(os.path.split(os.path.realpath(__file__))[0] + "/account.txt", "w") as f:
+                    f.write(self.account["username"] + "\n" + self.account["password"] + "\n")
         if not self.login():
             self.login()
-        self.sign()
+        if self.type == "刷课":
+            self.finish_media()   # 自动刷课，刷视频接口
+        elif self.type == "签到":
+            self.sign()
--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 import urllib3
-from Sign_xy import Sign_xy
+from __init__ import Sign_xy
+import sys
+
 
 if __name__ == "__main__":
     urllib3.disable_warnings()
+    type = None
+    tmp = sys.argv
+    times = None
+    if len(tmp) == 1 or tmp[1] == "-h":
+        print("用法：\npython3 main.py -s\t\t签到,如果后面跟数字，那么会每隔1分钟签到一次，持续你给定的次数停止\npython3 main.py -f\t\t刷视频或者音频等作业\npython3 main.py -h\t\t返回此帮助")
+    elif sys.argv[1].strip(" ") == "-s":
+        type = "签到"
+        if len(tmp) == 3:
+            times = sys.argv[2]
+    elif sys.argv[1].strip(" ") == "-f":
+        type = "刷课"
+    else:
+        print(
+            "用法：\npython3 main.py -s\t\t签到,如果后面跟数字，那么会每隔1分钟签到一次，持续你给定的次数停止\npython3 main.py -f\t\t刷视频或者音频等作业\npython3 main.py -h\t\t返回此帮助")
+
     mybot = Sign_xy()
+    mybot.type = type
+    mybot.times = times
     mybot.run()
