868e419ac6e665fcffff5610bb37faeddd3d686f
TwoOnefour <lys214412@gmail.com>
2023-12-15 13:03:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     tmp = sys.argv
     times = None
     if len(tmp) == 1 or tmp[1] == "-h":
-        print("用法：\npython3 main.py -s\t\t签到,如果后面跟数字，那么会每隔1分钟签到一次，持续你给定的次数停止\npython3 main.py -f\t\t刷视频或者音频等作业\npython3 main.py -h\t\t返回此帮助")
+        print("用法：\npython3 main.py -s\t\t签到,如果后面跟数字，那么会每隔1分钟签到一次，持续你给定的次数停止\npython3 main.py -f\t\t刷视频或者音频等作业\npython3 main.py -t\t\t刷时长\npython3 main.py -h\t\t返回此帮助")
     elif sys.argv[1].strip(" ") == "-s":
         type1 = "签到"
         if len(tmp) == 3:
